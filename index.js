75e395ee8cdf9105a17df4a51123cad798c77569
luovalley <luovalley@yandex.com>
2025-11-19 19:53:01 +0800

    Update configuration variables in index.js

--- index.js
+++ index.js
@@ -11,18 +11,29 @@ const UPLOAD_URL = process.env.UPLOAD_URL || '';      // 节点或订阅自动�
 const PROJECT_URL = process.env.PROJECT_URL || '';    // 需要上传订阅或保活时需填写项目分配的url,例如：https://google.com
 const AUTO_ACCESS = process.env.AUTO_ACCESS || false; // false关闭自动保活，true开启,需同时填写PROJECT_URL变量
 const FILE_PATH = process.env.FILE_PATH || './tmp';   // 运行目录,sub节点文件保存目录
-const SUB_PATH = process.env.SUB_PATH || 'sub';       // 订阅路径
+const SUB_PATH = process.env.SUB_PATH || 'sub0';       // 订阅路径
 const PORT = process.env.SERVER_PORT || process.env.PORT || 3000;        // http服务订阅端口
-const UUID = process.env.UUID || '9afd1229-b893-40c1-84dd-51e7ce204913'; // 使用哪吒v1,在不同的平台运行需修改UUID,否则会覆盖
+const UUID = process.env.UUID || '41f2c932-69aa-411a-b3eb-c012ba7f0faf'; // 使用哪吒v1,在不同的平台运行需修改UUID,否则会覆盖
 const NEZHA_SERVER = process.env.NEZHA_SERVER || '';        // 哪吒v1填写形式: nz.abc.com:8008  哪吒v0填写形式：nz.abc.com
 const NEZHA_PORT = process.env.NEZHA_PORT || '';            // 使用哪吒v1请留空，哪吒v0需填写
 const NEZHA_KEY = process.env.NEZHA_KEY || '';              // 哪吒v1的NZ_CLIENT_SECRET或哪吒v0的agent密钥
-const ARGO_DOMAIN = process.env.ARGO_DOMAIN || '';          // 固定隧道域名,留空即启用临时隧道
-const ARGO_AUTH = process.env.ARGO_AUTH || '';              // 固定隧道密钥json或token,留空即启用临时隧道,json获取地址：https://json.zone.id
+const ARGO_DOMAIN = process.env.ARGO_DOMAIN || 'node2.iconandy.dpdns.org';          // 固定隧道域名,留空即启用临时隧道
+const ARGO_AUTH = process.env.ARGO_AUTH || 'eyJhIjoiOWUxZjRhNjM5MmI1NzUxMjY2ZGMwMDBmMGY2OWU2MzAiLCJ0IjoiMGMyZmRlOWItMmI0OS00YzU5LWExNTItOWM5NzM0YTAzOTdmIiwicyI6IlpqZzBNMk5tTXpjdE1tRXlZUzAwTnpGaUxXSmlOVE10WWpFM1ptVmtOamt4TWpBeSJ9
+';              // 固定隧道密钥json或token,留空即启用临时隧道,json获取地址：https://json.zone.id
 const ARGO_PORT = process.env.ARGO_PORT || 8001;            // 固定隧道端口,使用token需在cloudflare后台设置和这里一致
-const CFIP = process.env.CFIP || 'cdns.doon.eu.org';        // 节点优选域名或优选ip  
-const CFPORT = process.env.CFPORT || 443;                   // 节点优选域名或优选ip对应的端口
-const NAME = process.env.NAME || '';                        // 节点名称
+const CFIP_LIST = [
+  "162.159.44.217",    // 香港 CN2 GIA，电信/联通神级
+  "162.159.38.79",  // 新加坡 163 骨干，联通晚高峰最稳
+  "162.159.39.137",    // 日本东京，移动/广电最优
+  "108.162.198.1",  // 香港备用，电信第二梯队
+  "162.159.49.10",   // 亚太混合优化，三网都能跑满
+  "162.159.41.238",    // 香港备用2，超稳
+  "162.159.35.136",    // 日本备用，抗封锁强
+  "172.64.47.51"   // 新加坡备用，峰值可达 200M+
+];
+const CFIP = process.env.CFIP || CFIP_LIST[Math.floor(Math.random() * CFIP_LIST.length)];
+const CFPORT = process.env.CFPORT || 443;
+const NAME = process.env.NAME || 'andyicon';                        // 节点名称
 
 // 创建运行文件夹
 if (!fs.existsSync(FILE_PATH)) {
